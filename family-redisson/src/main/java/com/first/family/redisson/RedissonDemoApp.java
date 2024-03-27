7adfded518383e9868149ba83aa696c6ce446a49
cuiweiman <daocaoren22222@163.com>
2024-03-27 09:49:50 +0800

    启动日志修改

--- family-redisson/src/main/java/com/first/family/redisson/RedissonDemoApp.java
+++ family-redisson/src/main/java/com/first/family/redisson/RedissonDemoApp.java
@@ -18,27 +18,25 @@ import java.util.Optional;
 @Slf4j
 @SpringBootApplication
 public class RedissonDemoApp {
-    public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(RedissonDemoApp.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringApplication.class, args);
         applicationContext.registerShutdownHook();
         ConfigurableEnvironment environment = applicationContext.getEnvironment();
-        try {
-            String ip = InetAddress.getLocalHost().getHostAddress();
-            String port = environment.getProperty("server.port");
-            String path = Optional
-                    .ofNullable(environment.getProperty("server.servlet.context-path")).orElse("");
-            String jdkVersion = (String) environment.getSystemProperties().get("java.version");
-            String jdkVm = (String) environment.getSystemProperties().get("java.vm.name");
-            String pid = (String) environment.getSystemProperties().get("PID");
-            log.info("\n-------------------------------------------------------------\n" +
-                    "\tJDK version: " + jdkVm + " " + jdkVersion + "\n" +
-                    "\tRedissonDemoApp is running! (^_^) PID: " + pid + "\n" +
-                    "\tLocal: \t\thttp://localhost:" + port + path + "/\n" +
-                    "\tExternal: \thttp://" + ip + ":" + port + path + "/\n" +
-                    // "Swagger: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
-                    "-------------------------------------------------------------");
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
+
+        String jdkVm = (String) environment.getSystemProperties().get("java.vm.name");
+        String jdkVersion = (String) environment.getSystemProperties().get("java.version");
+        String appName = environment.getProperty("spring.application.name");
+        String pid = (String) environment.getSystemProperties().get("PID");
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = environment.getProperty("server.port", "8080");
+        String path = Optional.ofNullable(environment.getProperty("server.servlet.context-path")).orElse("");
+
+        log.info("%n-------------------------------------------------------------%n"
+                        + "%t JDK version: {} {} %n" + "%t 应用 '{}' 启动成功!  (^_^)  PID: {} %n"
+                        + "%t 访问连接:%n"
+                        + "%t%t Knife4j 接口文档: %t%t http://{}:{}{}/doc.html%n"
+                        + "%t%t Druid 数据库监控: %t%t http://{}:{}{}/druid%n",
+                jdkVm, jdkVersion, appName, pid, ip, port, path, "127.0.0.1", port, path);
+
     }
 }
